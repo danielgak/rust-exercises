977e653310084fd4cbefaeaaefe33767a59c1376
Daniel Gak <dani_gak@hotmail.com>
2024-12-18 20:11:54 +0100

    feat: advent day 4

--- src/advent_2024/mod.rs
+++ src/advent_2024/mod.rs
@@ -3,6 +3,7 @@ mod day02;
 mod day02_tests;
 mod day03;
 mod day03_tests;
+mod day04;
 
 pub fn main() {
     // day01::problem_01_v1();
@@ -10,5 +11,7 @@ pub fn main() {
     // day02::problem_02_v1();
     // day02::problem_02_v2();
     // day03::problem_03_v1();
-    day03::problem_03_v2();
+    // day03::problem_03_v2();
+    // day04::problem_04_v1();
+    day04::problem_04_v2();
 }
